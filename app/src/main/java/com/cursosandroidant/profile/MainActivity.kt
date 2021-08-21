ab8e98c5f6f5fe770c73e426fa4f65a89cbe5496
Juan <juanftp100@gmail.com>
2021-08-21 18:53:01 -0400

    add: creacion de updateUI

--- app/src/main/java/com/cursosandroidant/profile/MainActivity.kt
+++ app/src/main/java/com/cursosandroidant/profile/MainActivity.kt
@@ -2,10 +2,28 @@ package com.cursosandroidant.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cursosandroidant.profile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateUI()
+    }
+
+    private fun updateUI(name:String="Cursos Android ANT"
+                         , email:String="cursosandroidant@gmail.com"
+                         , website:String="https://github.com/gallopelado"
+                         , phone:String="+52 555 673") {
+        binding.tvName.text = name
+        binding.tvEmail.text = email
+        binding.tvWebsite.text = website
+        binding.tvPhone.text = phone
+
     }
 }
