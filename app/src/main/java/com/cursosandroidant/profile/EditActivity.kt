88ba05365ab951b93422f52c5d130a5374712379
Juan <juanftp100@gmail.com>
2021-08-22 21:06:01 -0400

    add: buenas prácticas con Scope functions

--- app/src/main/java/com/cursosandroidant/profile/EditActivity.kt
+++ app/src/main/java/com/cursosandroidant/profile/EditActivity.kt
@@ -21,14 +21,22 @@ class EditActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         //Recibir el dato
-        with(binding){
+        intent.extras?.let {
+            binding.etName.setText(it.getString(getString(R.string.key_name)))
+            binding.etEmail.setText(it.getString(getString(R.string.key_email)))
+            binding.etWebsite.setText(it.getString(getString(R.string.key_website)))
+            binding.etPhone.setText(it.getString(getString(R.string.key_phone)))
+            binding.etLat.setText(it.getString(getString(R.string.key_lat)))
+            binding.etLong.setText(it.getString(getString(R.string.key_long)))
+        }
+        /*with(binding){
             etName.setText(intent.extras?.getString(getString(R.string.key_name)))
             etEmail.setText(intent.extras?.getString(getString(R.string.key_email)))
             etWebsite.setText(intent.extras?.getString(getString(R.string.key_website)))
             etPhone.setText(intent.extras?.getString(getString(R.string.key_phone)))
             etLat.setText(intent.extras?.getString(getString(R.string.key_lat)))
             etLong.setText(intent.extras?.getString(getString(R.string.key_long)))
-        }
+        }*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,6 +45,11 @@ class EditActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_save -> sendData()
+            android.R.id.home -> onBackPressed()
+        }
+        /*
         if(item.itemId == R.id.action_save) {
             // Destruye esta actividad
             //finish()
@@ -44,11 +57,23 @@ class EditActivity : AppCompatActivity() {
         } else if(item.itemId == android.R.id.home) {
             onBackPressed() // en vez de finish
         }
+         */
         return super.onOptionsItemSelected(item)
     }
 
     private fun sendData(){
         val intent = Intent()
+        with(binding){
+            intent.apply {
+                putExtra(getString(R.string.key_name), etName.text.toString())
+                putExtra(getString(R.string.key_email), etEmail.text.toString())
+                putExtra(getString(R.string.key_website), etWebsite.text.toString())
+                putExtra(getString(R.string.key_phone), etPhone.text.toString())
+                putExtra(getString(R.string.key_lat), etLat.text.toString())
+                putExtra(getString(R.string.key_long), etLong.text.toString())
+            }
+        }
+        /*
         with(intent) {
             putExtra(getString(R.string.key_name), binding.etName.text.toString())
             putExtra(getString(R.string.key_email), binding.etEmail.text.toString())
@@ -56,9 +81,8 @@ class EditActivity : AppCompatActivity() {
             putExtra(getString(R.string.key_phone), binding.etPhone.text.toString())
             putExtra(getString(R.string.key_lat), binding.etLat.text.toString())
             putExtra(getString(R.string.key_long), binding.etLong.text.toString())
-
-            setResult(RESULT_OK, intent)
-        }
+        }*/
+        setResult(RESULT_OK, intent)
         finish()
     }
 }
