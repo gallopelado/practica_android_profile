6a0acb61f969a5d0418152034ba9819a185eb443
Juan <juanftp100@gmail.com>
2021-10-02 16:52:35 -0400

    add: agregar fragment al activity

--- app/src/main/java/com/cursosandroidant/profile/SettingsActivity.kt
+++ app/src/main/java/com/cursosandroidant/profile/SettingsActivity.kt
@@ -2,10 +2,30 @@ package com.cursosandroidant.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            // otra manera distinta a poner el titulo en el android manifest
+            it.title = getString(R.string.settings_title)
+        }
+
+        // ligar/añadir el fragment al activity
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_main, SettingsFragment())
+            .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
